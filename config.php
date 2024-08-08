c39d016bccd1c00f435f075a1cf3bb719c50e899
WICKLES DE OLIVEIRA CORREIA <wickles.ocorreia@senacsp.edu.br>
2024-08-08 14:34:12 -0300

    criado config

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_servidor = 'localhost';
+$db_database = 'apipesseoas';
+$db_usuario = 'Wickles';
+$db_pwd = '12345678';
+
+$pdo = new PDO('mysql:host='.$db_servidor.';dbname='.$db_database,$db_usuario,$db_pwd);
+
+$array[] =[
+    'error' => "",
+    'result' => []
+]
+
+?>
