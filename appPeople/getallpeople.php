29bab91394e326cfe8a4cf1b2cd360f9e3733e3f
WICKLES DE OLIVEIRA CORREIA <wickles.ocorreia@senacsp.edu.br>
2024-08-15 16:43:25 -0300

    atualizando arquivo tirando comentarios e organizando a forma do resultado

--- appPeople/getallpeople.php
+++ appPeople/getallpeople.php
@@ -5,20 +5,13 @@ require('./../config.php');
 $metodo=strtoupper($_SERVER['REQUEST_METHOD']);
 
 if ($metodo==='GET') {
-    //selecionar tudo da tabela lembrete
+
     $sql=$pdo->query("SELECT * FROM pessoa");
 
-    // se o número de linhas do sql for maior que 0, vai executar
     if ($sql->rowCount()>0) {
 
-        // pegando tudo do sql e colocando na variável dados
-        //fetch_assoc: ler o array de forma associativa (CHAVE=VALOR)
-        $dados=$sql->fetchAll(PDO::FETCH_ASSOC);
-
-        var_dump($dados);
-
+        $array['result']=$sql->fetchAll(PDO::FETCH_ASSOC);
     }
-    //se o número de linhas não for maior que zero, mostrar mensagem de erro
     else{ 
         $array['error'] = 'Erro: Não há pessoas cadastradas';
     }
